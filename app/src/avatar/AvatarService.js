c2358d447ba2a6e3c9d56602b54e671589dfbc11
robinComa <robin.coma.public@gmail.com>
2015-02-21 00:39:29 +0100

    Add 6 users

--- app/src/avatar/AvatarService.js
+++ app/src/avatar/AvatarService.js
@@ -25,6 +25,46 @@ angular.module('avatars', []).service('AvatarService', function ($q){
         {
             who: 'Gani Ferrer',
             avatar: 'svg-6'
+        },
+        {
+            who: 'Antonio Banderas',
+            avatar: 'svg-7'
+        },
+        {
+            who: 'Alpha Blondie',
+            avatar: 'svg-8'
+        },
+        {
+            who: 'Miss Strong',
+            avatar: 'svg-9'
+        },
+        {
+            who: 'Danny the dog',
+            avatar: 'svg-10'
+        },
+        {
+            who: 'Cat Stevens',
+            avatar: 'svg-11'
+        },
+        {
+            who: 'Michelle Obama',
+            avatar: 'svg-12'
+        },
+        {
+            who: 'Bonnie Gates',
+            avatar: 'svg-13'
+        },
+        {
+            who: 'Bruce Lee',
+            avatar: 'svg-14'
+        },
+        {
+            who: 'Margarette Thatcher',
+            avatar: 'svg-15'
+        },
+        {
+            who: 'Bill Gates',
+            avatar: 'svg-16'
         }
     ];
 
